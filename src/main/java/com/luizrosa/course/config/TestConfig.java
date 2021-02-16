2da68a29a119023ec2f54088888876c259454848
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-16 14:25:28 -0300

    Payment, one-to-one association

--- src/main/java/com/luizrosa/course/config/TestConfig.java
+++ src/main/java/com/luizrosa/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luizrosa.course.entities.Category;
 import com.luizrosa.course.entities.Order;
 import com.luizrosa.course.entities.OrderItem;
+import com.luizrosa.course.entities.Payment;
 import com.luizrosa.course.entities.Product;
 import com.luizrosa.course.entities.User;
 import com.luizrosa.course.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3, oi4));	
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1); // usando o order para salvar o payment
 	}	
 }
