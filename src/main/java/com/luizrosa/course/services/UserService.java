f96281142d38d76003038ba193546e8a2da3780c
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-16 14:55:24 -0300

    User insert

--- src/main/java/com/luizrosa/course/services/UserService.java
+++ src/main/java/com/luizrosa/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) { // inserir no bd um novo objeto do tipo user
+		return repository.save(obj);
+	}
 }
